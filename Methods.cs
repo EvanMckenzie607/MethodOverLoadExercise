6b4538ced03ef9c62c7430a708e3e82d51c82282
Evan Mckenzie <615evan@gmail.com>
2022-08-16 12:29:18 -0400

    MethodoverloadingExercise Initial Commit

---
+++ Methods.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOeverloadExercise
+{
+    internal class Methods
+    {
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool isItDollars)
+        {
+            var sum = x + y; 
+
+            if (isItDollars == true && sum > 0)
+            {
+                return $"{sum} Dollars";
+            }
+            else if (isItDollars == true && sum == 1)
+            {
+                return $"{sum} Dollar";
+            }
+            else if (isItDollars == true && sum < 0)
+            {
+                return $"{sum} Dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+        public static string ADD(decimal x, decimal y, bool isItDollars)
+        {
+            var sum = x + y;
+
+            if (isItDollars == true && sum > 0)
+            {
+                return $"{sum} Dollars";
+            }
+            else if (isItDollars == true && sum == 1)
+            {
+                return $"{sum} Dollar";
+            }
+            else if (isItDollars == true && sum < 0)
+            {
+                return $"{sum} Dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using MethodOeverloadExercise;
+Console.WriteLine("How much money is in your two pockets, change included\n" +
+    "Please enter how much you have in have pocket below\n" +
+    "---------------------------------------------------");
+var pocket1 = int.Parse(Console.ReadLine());
+var pocket2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Methods.Add(pocket2, pocket1, true));
+
+
+
